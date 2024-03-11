82ef3e818af8d034155acc4ae239f0452a77c3d0
“Gonçalo <up202108773@edu.fe.up.pt>
2024-03-11 22:25:44 +0000

    Added normals to tangram.
    Co-authored-by: Andreia Silva <andreiasilva66@users.noreply.github.com>

--- tp3/old/MyParalellogram.js
+++ tp3/old/MyParalellogram.js
@@ -21,6 +21,17 @@ export class MyParalellogram extends CGFobject {
 			3, 1, 2
 		];
 
+		this.normals = [
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1
+		];
+
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
